e7b1f760f071e16e6ec6a2216a984169822f0cc4
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-18 13:53:55 +0545

    Values is done now

---
+++ values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func values() {
+	fmt.Println("go" + " " + "lang")
+	fmt.Println(1 + 1)
+	fmt.Println(true || false)
+	fmt.Println(true && false)
+	fmt.Println(!true)
+	fmt.Println(7 / 3)
+	fmt.Println(7.0 / 3.0)
+}
+
+func main() {
+	values()
+}
