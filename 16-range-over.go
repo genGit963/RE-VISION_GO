c91448a66e42dabd240e04906240282a2a769043
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-21 00:08:45 +0545

    16-range-over.go is done

--- 16-range-over.go
+++ 16-range-over.go
@@ -12,24 +12,27 @@ func learn_Range_Over() {
 	}
 	fmt.Println("sum : ", sum)
 
+	// also provide index of array or slices
 	for i, num := range nums {
 		if num == 3 {
 			fmt.Println("index: ", i)
 		}
 	}
 
-	// range for maps
+	// range for maps, destructure every key and value
 	kvs := map[string]string{"a": "apple", "b": "ball"}
 	for k, v := range kvs {
 		fmt.Printf("key: %s -> value:%s\n", k, v)
 	}
 
+	// range can also iterate over just the keys of a map.
 	for k := range kvs {
 		fmt.Println("keys: ", k)
 	}
 
-	for i, c := range "go lang is great" {
-		fmt.Printf("index i has: %d character: %c", i, c)
+	// range on strings iterates over Unicode code points.
+	for i, c := range "go Cool 😜" {
+		fmt.Printf("index i has: %d character: %c\n", i, c)
 	}
 
 }
