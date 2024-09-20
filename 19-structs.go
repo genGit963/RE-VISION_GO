37d6fb4f691a2fdeee3ec13b60faf470e8df2c91
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-21 01:25:01 +0545

    Working 19-structs.go

--- 19-structs.go
+++ 19-structs.go
@@ -2,8 +2,40 @@ package main
 
 import "fmt"
 
+/*
+Go’s structs are TYPED collections of FIELDS.
+They’re useful for grouping data together to form records.
+*/
+
+// structs declaration
+type person struct {
+	name string
+	age  int
+}
+
+func newPerson(name string) *person {
+	p := person{name: name}
+	p.age = 42
+	return &p
+}
+
 func learn_structs() {
 	fmt.Println("\n --------- learn Structs ---------")
+
+	// This syntax creates a new struct.
+	fmt.Println(person{"Bob", 20})
+
+	// You can name the fields when initializing a struct.
+	fmt.Println(person{name: "Alice", age: 30})
+
+	// Omitted fields will be zero-valued.
+	fmt.Println(person{name: "Fred"})
+
+	// An & prefix yields a pointer to the struct.
+	fmt.Println(&person{name: "Ann", age: 40})
+
+	// It’s idiomatic to encapsulate new struct creation in constructor functions
+	fmt.Println(newPerson("Jon"))
 }
 
 func main() {
