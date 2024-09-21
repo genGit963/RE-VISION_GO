0a1361c494c61453e48949b16b5c2be2ec7bd8eb
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-21 13:39:03 +0545

    22-enums.go learning

--- 22-enums.go
+++ 22-enums.go
@@ -2,8 +2,42 @@ package main
 
 import "fmt"
 
+type ServerState int
+
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "idle",
+	StateConnected: "connected",
+	StateError:     "error",
+	StateRetrying:  "retrying",
+}
+
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unknown state: %c", s))
+	}
+}
+
 func learn_Enums() {
 	fmt.Println("\n---------- Learn Enums -----------")
+
+	ns := transition(StateIdle)
+	fmt.Println(ns)
+	ns2 := transition(ns)
+	fmt.Println(ns2)
 
 }
 
