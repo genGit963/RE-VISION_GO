8c894006777acb0bacb4867de429d39a02098b62
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-22 14:02:48 +0545

    30-channels.go

--- 30-channels.go
+++ 30-channels.go
@@ -5,11 +5,29 @@ import "fmt"
 /*
 Channels are the pipes that connect concurrent goroutines.
 You can send values into channels from one goroutine and
+
 	receive those values into another goroutine.
 */
+func spacing() {
+	fmt.Println("\n---------- Learn Channels ----------")
+	fmt.Println()
+}
 
 func learn_Channels() {
-	fmt.Println("\n---------- Learn Channels ----------")
+	spacing()
+
+	/*
+		Create a new channel with make(chan val-type).
+		Channels are typed by the values they convey.
+	*/
+	messages := make(chan string)
+
+	go func() {
+		messages <- "ping go"
+	}()
+
+	msg := <-messages
+	fmt.Println(msg)
 
 }
 
