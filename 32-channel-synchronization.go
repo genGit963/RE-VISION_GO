e6a2035b45f5c434a12724e5149a1a87a0a7121d
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-22 14:25:07 +0545

    32-channel-synchronization.go: working

--- 32-channel-synchronization.go
+++ 32-channel-synchronization.go
@@ -17,9 +17,9 @@ func learn_Channel_Synchronization() {
 	done := make(chan bool, 1)
 	go worker(done)
 
-	if <-done {
-		fmt.Println("All work DONE !!")
-	}
+	// if <-done {
+	// 	fmt.Println("All work DONE !!")
+	// }
 }
 
 func main() {
