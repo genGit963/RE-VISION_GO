4f3934c3163479741d830a88d979a5fa58ec7fef
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-26 23:09:47 +0545

    44-atomic-counters.go: learning

--- 44-atomic-counters.go
+++ 44-atomic-counters.go
@@ -32,13 +32,18 @@ func learn_Atomic_counters() {
 
 		go func() {
 			for c := 0; c < 1000; c++ {
+				// To atomically increment the counter we use Add.
 				ops.Add(1)
 			}
 			wg.Done()
 		}()
 	}
 	wg.Wait()
-
+	/*
+		Here no goroutines are writing to ‘ops’, but using Load
+		it’s safe to atomically read a value even while other
+		goroutines are (atomically) updating it.
+	*/
 	fmt.Println("ops: ", ops.Load())
 
 }
