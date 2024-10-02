d131a0baa0e0f3f1671e738989d8c75850b5e96a
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-03 01:54:17 +0545

    Learn to design vscode snippet

---
+++ 55-regular-expressions.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+/*
+---> Regular Expression <---
+*/
+
+func learn_regular_expression() {
+	fmt.Println("\n------------ learn_regular_expression --------------")
+}
+
+func main() {
+	learn_regular_expression()
+	println("\n-------------------------------")
+}
