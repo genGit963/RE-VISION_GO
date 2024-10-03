2580c0d072c707d4b081d8d668b997d8a17a48f5
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-03 20:43:44 +0545

    57-XML.go

--- 57-XML.go
+++ 57-XML.go
@@ -38,12 +38,39 @@ func learn_XML() {
 	fmt.Println("\n------------ learn_XML --------------")
 	coffee := &Plant{Id: 17, Name: "coffee"}
 	coffee.Origin = []string{"Ethiopia", "Brazil"}
-
+	/*
+	   Emit XML representing our plant;
+	   using MarshalIndent to produce a more human-readable output.
+	*/
 	out, _ := xml.MarshalIndent(coffee, " ", " ")
 	fmt.Println(out)
 	fmt.Println(string(out))
-
+	// To add a generic XML header to the output, append it explicitly.
 	fmt.Println(xml.Header, string(out))
+
+	/*
+		Use Unmarshal to parse a stream of bytes with XML
+		into a data structure.
+
+		If the XML is malformed or cannot be mapped onto Plant,
+		a descriptive error will be returned.
+	*/
+	var p Plant
+	if err := xml.Unmarshal(out, &p); err != nil {
+		panic(err)
+	}
+	fmt.Println(p)
+	tomato := &Plant{Id: 81, Name: "Tomato"}
+	tomato.Origin = []string{"Mexico", "California"}
+
+	type Nesting struct {
+		XMLName xml.Name `xml:"nesting"`
+		Plants  []*Plant `xml:"parent>child>plant"`
+	}
+	nesting := &Nesting{}
+	nesting.Plants = []*Plant{coffee, tomato}
+	out, _ = xml.MarshalIndent(nesting, " ", "  ")
+	fmt.Println(string(out))
 }
 
 func main() {
