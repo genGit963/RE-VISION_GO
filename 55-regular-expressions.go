3b434f9522d735cc0558767afc646359baa3ce4e
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-03 19:04:51 +0545

    55-regular-expressions.go

--- 55-regular-expressions.go
+++ 55-regular-expressions.go
@@ -1,15 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
 /*
 ---> Regular Expression <---
+
 Go offers built-in support for regular expressions.
 Here are some examples of common regexp-related tasks in Go.
+
 */
 
 func learn_regular_expression() {
 	fmt.Println("\n------------ learn_regular_expression --------------")
+
+	match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
+	fmt.Print(match)
+
+	// r, _ := regexp
 }
 
 func main() {
