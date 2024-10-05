bf1304f9eefd5a4d35074156443006bc5dc47bc4
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-05 22:53:12 +0545

    67-writing-files.go

--- 67-writing-files.go
+++ 67-writing-files.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -31,28 +32,28 @@ func learn_writing_file() {
 	defer f.Close()
 
 	// You can Write byte slices as you’d expect.
-	d2 := "dkjafja"
-	// d2 := []byte{115, 45, 111, 109, 101, 10}
-	n2, err := f.Write(make([]byte, len(d2)))
+	// d2 := "dkjafja"
+	d2 := []byte{115, 45, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
 	checking(err)
 	fmt.Printf("wrote %d bytes\n", n2)
 
 	// A WriteString is also available.
-	//     n3, err := f.WriteString("writes\n")
-	//     checking(err)
-	//     fmt.Printf("wrote %d bytes\n", n3)
+	n3, err := f.WriteString("writes\n")
+	checking(err)
+	fmt.Printf("wrote %d bytes\n", n3)
 
 	// Issue a Sync to flush writes to stable storage.
-	//     f.Sync()
+	f.Sync()
 
 	// bufio provides buffered writers in addition to the buffered readers we saw earlier.
-	//     w := bufio.NewWriter(f)
-	//     n4, err := w.WriteString("buffered\n")
-	//     checking(err)
-	//     fmt.Printf("wrote %d bytes\n", n4)
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	checking(err)
+	fmt.Printf("wrote %d bytes\n", n4)
 
 	// Use Flush to ensure all buffered operations have been applied to the underlying writer.
-	// w.Flush()
+	w.Flush()
 }
 
 func main() {
