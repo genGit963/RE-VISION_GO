42e6a7df63dad39868b09a689df90e4dfa1fa94d
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-06 00:18:21 +0545

    71-temporary-files-and-directories.go

--- 71-temporary-files-and-directories.go
+++ 71-temporary-files-and-directories.go
@@ -29,24 +29,27 @@ func learn_temporary_files_and_directories() {
 	check(err)
 	/*
 	   Display the name of the temporary file.
-
 	   On Unix-based OSes the directory will likely be /tmp.
+
 	   The file name starts with the prefix given as the second argument
 	   to os.CreateTemp and
-
 	   the rest is chosen automatically to ensure that concurrent calls
 	   will always create different file names.
 	*/
 	fmt.Println("Temp file name:", f.Name())
-	// Clean up the file after we’re done. The OS is likely to clean up temporary files by itself after some time, but it’s good practice to do this explicitly.
 
-	//     defer os.Remove(f.Name())
+	/*
+		Clean up the file after we’re done.
+		The OS is likely to clean up temporary files by
+		itself after some time, but it’s good practice to do this explicitly.
+	*/
+	defer os.Remove(f.Name())
+
 	// We can write some data to the file.
+	_, err = f.Write([]byte{1, 2, 3, 4})
+	check(err)
 
-	//     _, err = f.Write([]byte{1, 2, 3, 4})
-	//     check(err)
 	// If we intend to write many temporary files, we may prefer to create a temporary directory. os.MkdirTemp’s arguments are the same as CreateTemp’s, but it returns a directory name rather than an open file.
-
 	//     dname, err := os.MkdirTemp("", "sampledir")
 	//     check(err)
 	//     fmt.Println("Temp dir name:", dname)
