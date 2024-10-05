fb801c69dda03df8b244ecb849d764aff1d70556
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-05 21:46:26 +0545

    63-url-parsing.go

--- 63-url-parsing.go
+++ 63-url-parsing.go
@@ -28,15 +28,16 @@ func learn_topic() {
 	// User contains all authentication info; call Username and Password on this for individual values.
 	fmt.Println(u.User)
 	fmt.Println(u.User.Username())
-	//     p, _ := u.User.Password()
-	//     fmt.Println(p)
+	p, _ := u.User.Password()
+	fmt.Println(p)
+
 	// The Host contains both the hostname and the port, if present. Use SplitHostPort to extract them.
 	//     fmt.Println(u.Host)
 	//     host, port, _ := net.SplitHostPort(u.Host)
 	//     fmt.Println(host)
 	//     fmt.Println(port)
-	// Here we extract the path and the fragment after the #.
 
+	// Here we extract the path and the fragment after the #.
 	//     fmt.Println(u.Path)
 	//     fmt.Println(u.Fragment)
 	// To get query params in a string of k=v format, use RawQuery. You can also parse query params into a map. The parsed query param maps are from strings to slices of strings, so index into [0] if you only want the first value.
