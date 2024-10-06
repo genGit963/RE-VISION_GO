be0e883f9dd152770de4de450f8aa1b555c8181a
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-06 21:40:31 +0545

    74-command-line-arguments

---
+++ 74-command-line-arguments.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+/*
+---> Command line arguments <---
+Command-line arguments are a common way to "parameterize execution of programs".
+
+For example, go run hello.go uses run and hello.go arguments to the go program.
+
+*/
+
+func learn_Command_line_argumet() {
+	fmt.Println("\n------------ learn_Command_line_argumet --------------")
+	/*
+		os.Args provides access to raw command-line arguments.
+
+		Note that the first value in this slice is the path to the program,
+		and os.Args[1:] holds the arguments to the program.
+	*/
+	argsWithProg := os.Args
+	argsWithoutProg := os.Args[1:]
+
+	// You can get individual args with normal indexing.
+	arg := os.Args[3]
+
+	fmt.Println(argsWithProg)
+	fmt.Println(argsWithoutProg)
+	fmt.Println(arg)
+}
+
+func main() {
+	learn_Command_line_argumet()
+	println("\n-------------------------------")
+}
