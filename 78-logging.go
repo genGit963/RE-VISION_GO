7599abd2b04b943d6d6ef9c205e45d2b1f012ced
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-07 00:11:04 +0545

    78-logging.go

--- 78-logging.go
+++ 78-logging.go
@@ -26,6 +26,20 @@ func learn_logging() {
 		Additional methods like Fatal* or Panic* will exit the program after logging.
 	*/
 	log.Println("standard logger")
+
+	/*
+		Loggers can be configured with flags to set their output format.
+
+		By default, the standard logger has the log.Ldate
+		and log.Ltime flags set, and these are collected in log.LstdFlags.
+
+		We can change its flags to emit time with microsecond accuracy,
+
+		for example.
+	*/
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	log.Println("with micro")
+
 }
 
 func main() {
